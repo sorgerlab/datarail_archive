bd5e3bb6d5e5637e8e0d6b979468158d78023b33
Gabriel F. Berriz <gabriel_berriz@hms.harvard.edu>
2014-03-24 11:45:47 -0400

    implement colex...
    ...and add documentation.

--- src/matlab/utils/cartesian_product.m
+++ src/matlab/utils/cartesian_product.m
@@ -1,6 +1,13 @@
-function P = cartesian_product(factors)
-%CARTESIAN_PRODUCT(FACTORS) computes the cartesian product of the elements
-%in the cell array FACTORS.
+function P = cartesian_product(factors, varargin)
+%CARTESIAN_PRODUCT compute the cartesian product of factor groups.
+%    CARTESIAN_PRODUCT(FACTORS) computes the cartesian product of the
+%    elements in the cell array FACTORS, in lexicographic order.
+%
+%    CARTESIAN_PRODUCT(FACTORS, COLEX) computes a cartesian product as
+%    before, but orders it colexicographically if COLEX is true.
+
+    narginchk(1, 2);
+    colex = nargin > 1 && varargin{1};
 
     if ~iscell(factors)
         error('argument is not a cell array');
@@ -15,9 +22,15 @@ function P = cartesian_product(factors)
         if nd == 1
             content = factors;
         else
-            tmp = fliplr(cellmap(@(c) 1:numel(c), factors));
+            tmp = cellmap(@(c) 1:numel(c), factors);
+            if ~colex
+                tmp = fliplr(tmp);
+            end
+            %tmp = fliplr(cellmap(@(c) 1:numel(c), factors));
             [idx{1:nd}] = ndgrid(tmp{:}); clear('tmp');
-            idx = fliplr(idx);
+            if ~colex
+                idx = fliplr(idx);
+            end
             content = arraymap(@(i) factors{i}(idx{i}(:)), 1:nd);
         end
         P = content;
--- src/matlab/utils/cartesian_product_table.m
+++ src/matlab/utils/cartesian_product_table.m
@@ -1,7 +1,16 @@
-function P = cartesian_product_table(factors, factornames)
-%CARTESIAN_PRODUCT_TABLE(FACTORS, FACTORNAMES) computes the cartesian
-%product of the elements in the cell array FACTORS and expresses the result
-%as a table with variable names as specified by FACTORNAMES.
+function P = cartesian_product_table(factors, factornames, varargin)
+%CARTESIAN_PRODUCT_TABLE compute the cartesian product of factor groups as
+%    a table.
+%
+%    CARTESIAN_PRODUCT_TABLE(FACTORS, FACTORNAMES) creates a table from the
+%    cartesian product of FACTORS, ordered lexicographically, and with
+%    column names given by FACTORNAMES.
+%
+%    CARTESIAN_PRODUCT_TABLE(FACTORS, FACTORNAMES, COLEX) creates a table
+%    as before but orders it colexicographically if COLEX is true.
+
+    narginchk(2, 3);
+    colex = nargin > 2 && varargin{1};
 
     if ~(iscell(factors) && iscell(factornames))
         error('first and/or second argument is not a cell array');
@@ -16,7 +25,7 @@ function P = cartesian_product_table(factors, factornames)
         P = table(0);
         P(:, 1) = []; % "empty 1-by-0 table"
     else
-        content = cartesian_product(factors);
+        content = cartesian_product(factors, colex);
         P = table(content{:, :}, 'VariableNames', factornames);
     end
 
