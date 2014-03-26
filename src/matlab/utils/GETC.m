7f2629a5d423b7eded0f10d237a4ea14ef9004c8
Gabriel F. Berriz <gabriel_berriz@hms.harvard.edu>
2014-03-26 10:01:07 -0400

    group_counts: minor fix

--- src/matlab/utils/GETC.m
+++ src/matlab/utils/GETC.m
@@ -2,5 +2,7 @@ function out = GETC( x, varargin )
 %GETC functional replacement for curly-brace index expressions.
 
     out = x{varargin{:}};
+    %out = builtin('_brace', ca, varargin{:});
+    %out = subsref(x, struct('type', '{}', 'subs', {varargin}));
 end
 
--- src/matlab/utils/group_counts.m
+++ src/matlab/utils/group_counts.m
@@ -1,14 +1,12 @@
 function out = group_counts(tbl, varargin)
 %GROUP_COUNTS count numbers of rows in groupings.
 %
-%     T2 = GROUP_COUNTS(T1, KEYVARS)
+%     T2 = GROUP_COUNTS(T1 [, KEYVARS])
 % 
-%     T2 = GROUP_COUNTS(T1)
-%
 %     Returns a table whose columns correspond to KEYVARS plus an
 %     additional column holding the numbers of rows in T1 having each
 %     available combination of values of the variables in KEYVARS.
-
+%
 %     If KEYVARS is omitted, the default is computed as described in
 %     the documentation for the KEYVARS parameter of the function
 %     TABLE_TO_NDARRAY.
@@ -20,28 +18,12 @@ function out = group_counts(tbl, varargin)
     else
         params = {};
     end
-      
-    args = [{tbl} params];
+    [tbl, kvs, ~, ~, ~] = process_args__({'KeyVars'}, [{tbl} params]);
 
-    [tbl, kns, ~, ~, ~] = ...
-        process_args__({'KeyVars'}, args);
-
-    kis = dr.vidxs(tbl, kns);
-
-    vv = 'counts';
-    try
-        kvs = tbl.Properties.VariableNames(1, kis);
-        vns = matlab.lang.makeUniqueStrings([kvs {vv}]);
-        vv = vns{end};
-    catch e
-        if ~strcmp(e.identifier, 'MATLAB:undefinedVarOrClass')
-            rethrow(e);
-        end
-        vv = genvarname(vv, kvs);
-    end
-        
+    kis = dr.vidxs(tbl, kvs);
     t = tbl(:, kis);
+    vv = genvarname_(kvs, 'counts');
     t.(vv) = ones(height(t), 1);
 
-    out = collapse_(t, @numel, kns, {vv});
+    out = collapse_(t, @numel, kvs, {vv});
 end
