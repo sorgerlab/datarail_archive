3b23639bc718deee260fa72f16a62048e720747c
Gabriel F. Berriz <gabriel_berriz@hms.harvard.edu>
2014-04-02 15:13:50 -0400

    refactor varnames

---
+++ src/matlab/utils/private/nr_.m
@@ -0,0 +1,23 @@
+function [] = nr_(seq)
+    if istable(seq)
+        n = height(seq);
+        seq.Properties.RowNames = arraymap(@num2str, 1:n);
+        todisp = seq;
+    else
+        n = length_(seq);
+        fmt = sprintf('%%%dd\t%%s', floor(log10(n)) + 1);
+        seq = tostr_(seq, n);
+        lines = arraymap(@(i) sprintf(fmt, i, seq{i}), 1:n);
+        todisp = strjoin(lines, '\n');
+    end
+    disp(todisp);
+end
+
+function out = tostr_(seq, n)
+    seq = reshape(seq, n, []);
+    if iscell(seq)
+        out = arraymap(@(i) num2str(seq{i, :}), 1:n);
+    else
+        out = arraymap(@(i) num2str(seq(i, :)), 1:n);
+    end    
+end
--- src/matlab/utils/varnames.m
+++ src/matlab/utils/varnames.m
@@ -2,17 +2,10 @@ function varargout = varnames(tbl)
     vns = tbl.Properties.VariableNames;
     if nargout == 0
         varargout = {};
-        dispvns_(vns);
+        nr_(vns);
     elseif nargout == 1
         varargout = {vns};
     else
         varargout = vns;
     end
-end
-
-function dispvns_(vns)
-    n = numel(vns);
-    fmt = sprintf('%%%dd\t%%s', floor(log10(n)) + 1);
-    lines = arraymap(@(i) sprintf(fmt, i, vns{i}), 1:n);
-    disp(strjoin(lines, '\n'));
 end
