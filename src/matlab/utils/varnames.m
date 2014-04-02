bbc967fec8f3e929006e8992111445eb33009a01
Gabriel F. Berriz <gabriel_berriz@hms.harvard.edu>
2014-04-02 15:25:01 -0400

    make varnames a setter

--- src/matlab/utils/varnames.m
+++ src/matlab/utils/varnames.m
@@ -1,11 +1,17 @@
-function varargout = varnames(tbl)
-    vns = tbl.Properties.VariableNames;
-    if nargout == 0
-        varargout = {};
-        nr_(vns);
-    elseif nargout == 1
-        varargout = {vns};
+function varargout = varnames(tbl, varargin)
+    narginchk(1, 2);
+    if nargin > 1
+        tbl.Properties.VariableNames = varargin{1};
+        varargout = {tbl};
     else
-        varargout = vns;
+        vns = tbl.Properties.VariableNames;
+        if nargout == 0
+            varargout = {};
+            nr_(vns);
+        elseif nargout == 1
+            varargout = {vns};
+        else
+            varargout = vns;
+        end
     end
 end
