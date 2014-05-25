e74fca9438849d48e7f20456aa24d54ef9e5566c
Gabriel F. Berriz <gabriel_berriz@hms.harvard.edu>
2014-05-24 20:07:20 -0400

    collapse: add docs for Irregular param

--- src/matlab/utils/collapse.m
+++ src/matlab/utils/collapse.m
@@ -5,12 +5,29 @@ function out = collapse(tbl, aggrs, varargin)
 %
 %     T2 = COLLAPSE(T1, AGGRS, 'PARAM1',val1, 'PARAM2',val2, ...) allows
 %     you to specify optional parameter name/value pairs to control
-%     COLLAPSE's behavior.  These parameters are listed below, and their
-%     documentation is identical to those of the same-named parameters for
-%     the function TABLE_TO_NDARRAY.
+%     COLLAPSE's behavior.  These parameters are listed below.  The
+%     documentation of the first two is identical to those of the
+%     same-named parameters for the function TABLE_TO_NDARRAY.
 %
 %         'KeyVars'
 %         'ValVars'
+%       'Irregular' - Should be either a cell array of boolean values
+%                     having as many elements as there are value variables,
+%                     or a single boolean variable.  (The second form is a
+%                     shorthand for a value in the first form where all the
+%                     boolean entries have the value specified.)  The
+%                     boolean value corresponding to each value variable
+%                     determines how the collapsed values for that variable
+%                     will be stacked vertically to form a column of the
+%                     returned table.  When this value is TRUE, the entries
+%                     will be treated as irregular, and stacked as a
+%                     vertical cell array having variably-sized entries.
+%                     Otherwise, the entries will be expected to have
+%                     uniform dimensions orthogonal to their first
+%                     dimension, and therefore suitable for stacking with
+%                     VERTCAT.
+%
+%                     DEFAULT: FALSE.
 
     narginchk(2, 6);
     args = [{tbl} varargin];
